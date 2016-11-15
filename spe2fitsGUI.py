41f57100b633efa631b5276bf684f165024b2100
jerryjia <jerryjiahaha@gmail.com>
2016-11-15 23:04:51 +0800

    monitor a directory to perform convertion automatically

--- spe2fitsGUI.py
+++ spe2fitsGUI.py
@@ -13,8 +13,15 @@ image sub
 import sys
 import os
 import traceback
+import re
+from functools import partial
 from pathlib import Path # New in version 3.4
 
+# ref: https://github.com/gorakhargosh/watchdog
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# ref: http://effbot.org/tkinterbook/
 import tkinter as tk
 from tkinter import filedialog, messagebox, Checkbutton
 from tkinter import ttk
@@ -23,6 +30,29 @@ from tkinter import ttk
 
 #sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from spe2fits import SPE
+
+class customerDirHandler(FileSystemEventHandler):
+    SPE_FILE_PATTERN = re.compile(".*\.spe", re.IGNORECASE)
+    def __init__(self, hook = None):
+        self._hook = hook
+
+    def set_hook(self, hook):
+        self._hook = hook
+
+    def on_created(self, event):
+        super().on_created(event)
+        print("on created", event)
+        if event.is_directory:
+            return
+        newfile = os.path.abspath(event.src_path)
+        print("newfile", newfile)
+        if not customerDirHandler.SPE_FILE_PATTERN.match(newfile):
+            return
+        try:
+            print("process hook")
+            self._hook(newfile)
+        except Exception as e:
+            print(e)
 
 def yieldFilesUnderDirectory(dirname, match = None):
     """ yield all file names under the directory 'dirname' recursively
@@ -78,15 +108,18 @@ class Application(tk.Frame):
                 offsety = int(height/4) if height > 600 else 80,
                 )
 
-
     def createWidgets(self):
+
+        # settings of file overwrite
         self.overWriteFileFlag = tk.BooleanVar(self)
-        self.overwriteFile = tk.Checkbutton(self.parent,
+        self.overwriteFile = tk.Checkbutton(self,
                 text = "overwrite exist",
                 variable = self.overWriteFileFlag,
                 onvalue = True, offvalue = False,
                 )
         self.overwriteFile.place(x=50, y=150)
+
+        # button for select files to convert
         self.chooseFile = tk.Button(self,
                 text = "Convert Files...",
                 fg="black", bg="white",
@@ -96,6 +129,7 @@ class Application(tk.Frame):
         self.chooseFilePath = os.path.abspath(os.path.curdir)
         self.chooseFileOutDir = self.chooseFilePath
 
+        # button for select directory to convert
         self.chooseDir = tk.Button(self,
                 text = "Convert Directory...",
                 fg="black", bg="white",
@@ -105,12 +139,134 @@ class Application(tk.Frame):
         self.chooseDirPath = os.path.abspath(os.path.curdir)
         self.chooseDirOutDir = self.chooseDirPath
 
+        # select dir to listen
+        ## source direcory
+        self.listenDirPath = tk.StringVar(
+                master = self, value = os.path.abspath(os.path.curdir),
+                name = "listenDirPath",
+                )
+        ## destination directory
+        self.listenDirOutputPath = tk.StringVar(
+                master = self, value = os.path.abspath(os.path.curdir),
+                name = "listenDirOutputPath",
+                )
+        self.listenDir = tk.Button(self,
+                text = "Monitor directory...",
+                fg="black", bg="white",
+                command = self.addListenDir,
+                )
+        self.listenDir.place(x=50, y=250)
+        self.listenDirOutput = tk.Button(self,
+                text = "Binded directory...",
+                fg="black", bg="white",
+                command = self.bindListenDir,
+                )
+        self.listenDirOutput.place(x=50, y=300)
+        self.listenDirEnableFlag = tk.BooleanVar(self)
+        self.listenDirEnable = tk.Checkbutton(self,
+                text = "Enable monitor",
+                variable = self.listenDirEnableFlag,
+                command = self.checkListenTask,
+                onvalue = True, offvalue = False,
+                )
+        self.listenDirEnable.place(x=50, y=350)
+        self.listenDirPathShow = tk.Label(self,
+                fg="black", bg="white", width = 80,
+                textvariable = self.listenDirPath,
+                )
+        self.listenDirPathShow.place(x=60+self.listenDir.winfo_reqwidth(),
+                y=250)
+        self.listenDirOutputPathShow = tk.Label(self,
+                fg="black", bg="white", width = 80,
+                textvariable = self.listenDirOutputPath,
+                )
+        self.listenDirOutputPathShow.place(x=60+self.listenDir.winfo_reqwidth(),
+                y=300)
+
+    def checkListenTask(self):
+        self.listener.unschedule_all()
+        print("checkListenTask")
+        if self.listenDirEnableFlag.get():
+            print("enable")
+            listen_dir = os.path.abspath(self.listenDirPath.get())
+            try:
+                self.listener.schedule(self._listen_handler,
+                        listen_dir, recursive = True)
+            except Exception as e:
+                messagebox.showinfo("listen dir {dirname} failed"
+                        .format(dirname = listen_dir),
+                        "{reason}".format(
+                            reason = traceback.format_exception(*sys.exc_info())
+                            )
+                        )
+        else:
+            print("disable")
+
+    def addListenDir(self):
+        print("addListenDir")
+        listenDirPath = filedialog.askdirectory(
+                parent = self,
+                title = "Auto convert (listen) .spe under this directory",
+                initialdir = self.listenDirPath.get(),
+                )
+        if not self.checkDir(listenDirPath, autocreate = False, poperror = False):
+            return
+        print("listen:", listenDirPath)
+        if os.path.abspath(listenDirPath) != self.listenDirPath.get():
+            self.listenDirPath.set(os.path.abspath(listenDirPath))
+            self.checkListenTask()
+
+    def bindListenDir(self):
+        print("bindListenDir")
+        newOutDir = filedialog.askdirectory(
+                parent = self,
+                title = "Auto convert .spe into this directory",
+                initialdir = self.listenDirPath.get(),
+                )
+        if not self.checkDir(newOutDir, autocreate = False, poperror = False):
+            return
+        print("bind to:", newOutDir)
+        self.listenDirOutputPath.set(os.path.abspath(newOutDir))
+
+    @property
+    def listener(self):
+        """ observer of files, for listening file created event
+        """
+        if not hasattr(self, "_listener"):
+            self._listener = Observer()
+            self._listen_handler = customerDirHandler(self.on_created)
+            self._listener.start()
+        return self._listener
+
+    def on_created(self, onefile):
+        if not self.listenDirEnableFlag.get():
+            return
+        try:
+            print("convert:", onefile)
+            outputDir = self.listenDirOutputPath.get()
+            oldPrefix = self.listenDirPath.get()
+            outPrefix = getOutputPrefix( onefile,
+                    outputDir, oldPrefix)
+            print("output prefix:", outPrefix)
+            self.checkDir(os.path.dirname(outPrefix))
+            speHandler = SPE(onefile)
+            speHandler.spe2fits(
+                    outPrefix = outPrefix,
+                    clobber = self.overWriteFileFlag.get(),
+                    )
+        except Exception as e:
+            messagebox.showinfo("Convert " + onefile + " Failed" + str(e),
+                    traceback.format_exception(*sys.exc_info()))
+        else:
+            # TODO show status on widgets
+            print("created")
+
     def chooseDialogFiles(self):
         print("chooseDialogFiles")
         filenames = filedialog.askopenfilenames(
                 defaultextension = '.SPE',
                 filetypes = [('WinViewer Documents', '.SPE'), ('all files', '.*'),],
-                parent = self.parent,
+                parent = self,
                 title = "Select .SPE files",
                 multiple = True,
                 )
@@ -123,10 +279,11 @@ class Application(tk.Frame):
         print("select:", self.chooseFilePath)
         self.chooseFileOutDir = filedialog.askdirectory(
                 initialdir = self.chooseFilePath,
-                parent = self.parent,
+                parent = self,
                 title = "Select output Directory",
                 mustexist = False,
                 )
+        print("out:", self.chooseFileOutDir)
         if not self.checkDir(self.chooseFileOutDir):
             return
         self.convertFiles(filenames, self.chooseFileOutDir)
@@ -135,7 +292,7 @@ class Application(tk.Frame):
         print("chooseDialogDir")
         self.chooseDirPath = filedialog.askdirectory(
                 initialdir = self.chooseDirPath,
-                parent = self.parent,
+                parent = self,
                 title = "Select Directory contains .SPE files",
                 )
         print("select:", self.chooseDirPath)
@@ -143,7 +300,7 @@ class Application(tk.Frame):
             return
         self.chooseDirOutDir = filedialog.askdirectory(
                 initialdir = self.chooseDirPath,
-                parent = self.parent,
+                parent = self,
                 title = "Select output Directory",
                 mustexist = False,
                 )
@@ -158,13 +315,16 @@ class Application(tk.Frame):
         self.convertFiles(filenames, self.chooseDirOutDir,
                 oldPrefix = self.chooseDirPath)
 
-    def checkDir(self, dirname):
+    def checkDir(self, dirname, autocreate = True, poperror = True):
+        if dirname is None or len(dirname) == 0:
+            return False
         try:
-            if not os.path.exists(dirname):
+            if not os.path.exists(dirname) and autocreate:
                 os.makedirs(dirname)
         except Exception as e:
-            messagebox.showinfo("Check Directory Failed: " + str(e),
-                    traceback.format_exception(*sys.exc_info()))
+            if poperror:
+                messagebox.showinfo("Check Directory Failed: " + str(e),
+                        traceback.format_exception(*sys.exc_info()))
             return False
         return True
 
@@ -184,7 +344,7 @@ class Application(tk.Frame):
                 speHandler = SPE(onefile)
                 outPrefix = getOutputPrefix( onefile,
                         outputDir, oldPrefix )
-                print(outPrefix)
+                print("output prefix:", outPrefix)
                 self.checkDir(os.path.dirname(outPrefix))
                 # TODO check file existence first
                 # TODO make it async
@@ -206,6 +366,12 @@ class Application(tk.Frame):
                     filecount = filecount, outputDir = outputDir,
                     allcount = fileallcount)
                 )
+    def cleanup(self):
+        print("cleanup")
+        self.listener.stop()
+        self.listener.join()
+        self.parent.destroy()
+
 
 def workaroundForGUI():
     """ redirect I/O when in GUI
@@ -234,10 +400,15 @@ def workaroundForGUI():
         sys.stdout = sys.stderr = sys.stdin = sys.__stdout__ = sys.__stdin__ = sys.__stderr__ = GuiLogger()
 
 
+def on_closing(app):
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        app.cleanup()
+
 def main():
     root = tk.Tk()
     workaroundForGUI()
     app = Application(master = root)
+    root.protocol("WM_DELETE_WINDOW", partial(on_closing, app))
     root.mainloop()
 
 if __name__ == '__main__':
